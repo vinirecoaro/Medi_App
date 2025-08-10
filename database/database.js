571c2e4052097625a77d719309367a842d68a9ab
Vinicius Recoaro <vrecoaro@hotmail.com>
2025-08-10 11:31:37 -0300

    Configured controllers routers

--- database/database.js
+++ database/database.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose"
 
 mongoose.connect('mongodb://localhost:27017/medi-app')
 
-const bd = mongoose.connection
+const db = mongoose.connection
 
 db.on('error', console.error.bind(console, 'connection error: '))
 
