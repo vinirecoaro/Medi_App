08dbb8e9a4ea2f893b36a55da056e41c1e2d5106
Vinicius Recoaro <vrecoaro@hotmail.com>
2025-08-21 18:16:52 -0300

    added login route

--- routes/router.js
+++ routes/router.js
@@ -3,6 +3,10 @@ import appointmentController from "./AppointmentController.js"
 import doctorController from "./DoctorController.js"
 import patientController from "./PatientController.js"
 import prescriptionController from "./PrescriptionController.js"
+import doctorService from "../services/DoctorService.js"
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken'
+import verifyToken from "../middleware/authMiddleware.js"
 
 let router = express.Router()
 
@@ -13,9 +17,36 @@ router.get(
     }
 )
 
-router.use("/", appointmentController)
-router.use("/", doctorController)
-router.use("/", patientController)
-router.use("/", prescriptionController)
+//mapeamento do login
+router.post('/login', async (req, res) => {
+    try {
+        const {login, password} = req.body
+        const doctor = await doctorService.getDoctorByLogin(login)
+        if(!doctor){
+            return res.status(401).json({error: 'Authentication failed!'})
+        }
+
+        const passwordMatch = await bcrypt.compare(password, doctor.password)
+        if(!passwordMatch){
+            return res.status(401).json({error: 'Authentication failed!'})
+        }
+
+        const token = jwt.sign({doctorId: doctor._id}, 'you-secret-key', {expiresIn: '1h'})
+        res.status(200).json({token})
+    } catch (error) {
+        console.log(error)
+        res.status(500).json({error: 'Login failed!'})
+    }
+})
+
+router.use("/", verifyToken, appointmentController)
+router.use("/", verifyToken, doctorController)
+router.use("/", verifyToken, patientController)
+router.use("/", verifyToken, prescriptionController)
+
+// router.use("/", appointmentController)
+// router.use("/", doctorController)
+// router.use("/", patientController)
+// router.use("/", prescriptionController)
 
 export default router
